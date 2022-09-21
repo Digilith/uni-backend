624c0715da82f6d566d41069de8daaccc82157f4
Elina <eliboc@inbox.ru>
2022-09-22 02:02:02 +0300

    Drawer

--- 2/php/src/drawer.php
+++ 2/php/src/drawer.php
@@ -4,12 +4,17 @@
         private $shape;
         // 0 - red, 1 - blue
         private $color;
-        // 1 - 9
+        // 1 - 10
         private $size;
 
         function parseGet(){
             // 3-digit num
             $getQuery = $_GET['num'] % 1000;
+            if ($getQuery < 10) {
+                $getQuery *= 100;
+            } elseif ($getQuery < 100) {
+                $getQuery *= 10;
+            }
 
             // 0 or 1
             $this->shape = $getQuery / 100 % 2;
@@ -23,20 +28,34 @@
 
             // size
             $this->size = $getQuery % 10 + 1;
+
+            echo 'parseGet';
         }
 
         function draw(){
             $tag = "";
-
+            
             if($this->shape == 0){
-                $tag = "<rect width='$this->size' height='$this->size fill='$this->color'/>";
+                $tag = "<rect x='$this->size' y='$this->size' width='$this->size' height='$this->size' fill='$this->color'/>";
             } else {
                 $center = $this->size + 10;
-                $tag = "<circle cx='$center' cy='$center r='$this->size' fill='$this->color'/>";
+                $tag = "<circle cx='$center' cy='$center' r='$this->size' fill='$this->color'/>";
             }
 
+            echo 'draw';
             return $tag;
         }
 
     }
+
+
+    $drawer = new Drawer();
+    $drawer->parseGet();
+
+    $svg_begin = '<svg viewBox="0 0 100 100">';
+    $svg_end   = '</svg>';
+
+    echo $svg_begin;
+    echo $drawer->draw();
+    echo $svg_end;
 ?>
