5423e1edd6055ca8a8e1cd971bdb9c841b57dc23
Elina <eliboc@inbox.ru>
2022-10-13 13:09:39 +0300

    init sql

---
+++ 3/mysql/init.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE OF NOT EXISTS bookstore;
+CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'password';
+GRANT SELECT,UPDATE,INSERT ON bookstore;
+FLUSH PRIVELEGES;
+
+USE bookstore;
+CREATE TABLE IF NOT EXISTS user (
+    ID INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128) NOT NULL,
+    surname VARCHAR(128) NOT NULL,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE IF NOT EXISTS auth (
+    ID INT(11) NOT NULL AUTO_INCREMENT,
+    login VARCHAR(20) NOT NULL UNIQUE,
+    password VARCHAR(40) NOT NULL,
+    PRIMARY KEY(ID)
+);
+
+INSERT INTO auth (login, password) VALUES ('login', 'password');
+
+INSERT INTO user (name, surname)
+VALUES
+    ('John', 'Smith'),
+    ('Jane', 'Doe'),
+    ('Mary', 'Sue'),
+    ('Saul', 'Goodman');
